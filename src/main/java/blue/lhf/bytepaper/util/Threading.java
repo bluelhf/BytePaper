6e85d37d0482f394da66f23a5549a550c08bcb20
bluelhf <ilari.suhonen@gmail.com>
2022-06-19 20:40:23 +0300

    feat: threading utils

---
+++ src/main/java/blue/lhf/bytepaper/util/Threading.java
@@ -0,0 +1,20 @@
+package blue.lhf.bytepaper.util;
+
+import net.minecraft.server.MCUtil;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+public class Threading {
+    private Threading() {
+
+    }
+
+    public static <T> T forceMain(Supplier<T> tSupplier) {
+        return CompletableFuture.supplyAsync(tSupplier, MCUtil.MAIN_EXECUTOR).join();
+    }
+
+    public static void forceMain(Runnable runnable) {
+        CompletableFuture.runAsync(runnable, MCUtil.MAIN_EXECUTOR).join();
+    }
+}
