c0291481088f7873fc86e4e43c7ede30cfce80c3
bluelhf <blueloveshisfriends@gmail.com>
2022-07-16 15:04:08 +0300

    fix: box last login player property
    explicitly defines last login player property in order to auto-box it properly — this should be done for all supported properties in the future

--- src/main/java/blue/lhf/bytepaper/library/syntax/player/PlayerProperties.java
+++ src/main/java/blue/lhf/bytepaper/library/syntax/player/PlayerProperties.java
@@ -1,8 +1,21 @@
 package blue.lhf.bytepaper.library.syntax.player;
 
-import blue.lhf.bytepaper.util.property.PropertyHolder;
+import blue.lhf.bytepaper.util.property.*;
 import org.bukkit.entity.Player;
+import org.byteskript.skript.lang.handler.StandardHandlers;
 
+@SuppressWarnings("unused")
 @PropertyHolder(owner = Player.class, implicit = true)
 public class PlayerProperties {
+    private PlayerProperties() {
+    }
+    @Property(name = "last login", type = Long.class)
+    public static class PropLastLogin {
+        private PropLastLogin() {
+        }
+        @PropertyActor(StandardHandlers.GET)
+        public static Long get(Player player) {
+            return player.getLastLogin();
+        }
+    }
 }
