e9bda63e68cf319b573d08c9337a24faba4ed51e
bluelhf <blueloveshisfriends@gmail.com>
2022-07-16 15:10:36 +0300

    revert: disable debugging by default

--- src/main/java/blue/lhf/bytepaper/BytePaper.java
+++ src/main/java/blue/lhf/bytepaper/BytePaper.java
@@ -35,7 +35,7 @@ public final class BytePaper extends JavaPlugin implements IScriptLoader {
 
     @Override
     public void onEnable() {
-        register(Debugging.BOTH);
+        register(Debugging.OFF);
     }
 
     @SuppressWarnings("unused")
