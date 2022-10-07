e839b8fe7b7dfff61c1c84da4e12507ee2237e69
bluelhf <ilari.suhonen@gmail.com>
2022-10-07 17:43:58 +0300

    chore(docs): document broadcast effect
    Signed-off-by: bluelhf <ilari.suhonen@gmail.com>

--- src/main/java/blue/lhf/bytepaper/library/syntax/chat/EffectBroadcast.java
+++ src/main/java/blue/lhf/bytepaper/library/syntax/chat/EffectBroadcast.java
@@ -4,19 +4,32 @@ import blue.lhf.bytepaper.library.syntax.SyntaxUtils;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.byteskript.skript.api.Library;
+import org.byteskript.skript.api.note.Documentation;
 import org.byteskript.skript.api.syntax.Effect;
-import org.byteskript.skript.compiler.CompileState;
-import org.byteskript.skript.compiler.Context;
-import org.byteskript.skript.compiler.Pattern;
+import org.byteskript.skript.compiler.*;
 import org.byteskript.skript.lang.element.StandardElements;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static mx.kenzie.foundation.WriteInstruction.*;
 
+@Documentation(
+    name = "Broadcast",
+    description = "Broadcasts the given message to all players and the console.\n",
+    examples = {
+        """
+            on script load:
+                trigger:
+                    broadcast mini "<rainbow>Some rainbow text!</rainbow>"
+                    """,
+        """
+            on script load:
+                trigger:
+                    broadcast message "Some plain text."
+            """
+    }
+)
 public class EffectBroadcast extends Effect {
     public EffectBroadcast(Library library) {
         super(library, StandardElements.EFFECT, "broadcast [message] %Object%");
