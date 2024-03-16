aee0f3e70e411ee6b5f73c6fa219fafc2d7703fa
bluelhf <ilari.suhonen@gmail.com>
2024-03-16 16:14:46 +0200

    feat: update to 1.20.4

--- src/main/java/blue/lhf/bytepaper/library/syntax/block/BlockProperties.java
+++ src/main/java/blue/lhf/bytepaper/library/syntax/block/BlockProperties.java
@@ -1,12 +1,15 @@
 package blue.lhf.bytepaper.library.syntax.block;
 
-import blue.lhf.bytepaper.util.Threading;
 import blue.lhf.bytepaper.util.property.Property;
 import blue.lhf.bytepaper.util.property.PropertyActor;
 import blue.lhf.bytepaper.util.property.PropertyHolder;
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.byteskript.skript.lang.handler.StandardHandlers;
+
+import java.util.concurrent.ExecutionException;
 
 @SuppressWarnings("unused")
 @PropertyHolder(owner = Block.class)
@@ -17,13 +20,19 @@ public class BlockProperties {
     public static class PropData {
         private PropData() {}
         @PropertyActor(StandardHandlers.GET)
-        public static BlockData get(Block block) {
-            return Threading.forceMain(block::getBlockData);
+        public static BlockData get(Block block) throws ExecutionException, InterruptedException {
+            return Bukkit.getScheduler().callSyncMethod(
+                    JavaPlugin.getProvidingPlugin(BlockProperties.class),
+                    block::getBlockData
+            ).get();
         }
 
         @PropertyActor(StandardHandlers.SET)
         public static void set(Block block, BlockData data) {
-            Threading.forceMain(() -> block.setBlockData(data));
+            Bukkit.getScheduler().runTask(
+                    JavaPlugin.getProvidingPlugin(BlockProperties.class),
+                    () -> block.setBlockData(data)
+            );
         }
     }
 }
--- src/main/java/blue/lhf/bytepaper/library/syntax/entity/ExprEntities.java
+++ src/main/java/blue/lhf/bytepaper/library/syntax/entity/ExprEntities.java
@@ -1,16 +1,17 @@
 package blue.lhf.bytepaper.library.syntax.entity;
 
-import blue.lhf.bytepaper.util.Threading;
-import io.papermc.paper.util.MCUtil;
 import mx.kenzie.foundation.Type;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.byteskript.skript.api.Library;
 import org.byteskript.skript.api.syntax.SimpleExpression;
 import org.byteskript.skript.lang.element.StandardElements;
 import org.byteskript.skript.lang.handler.StandardHandlers;
 import org.byteskript.skript.runtime.Skript;
+
+import java.util.concurrent.ExecutionException;
 
 public class ExprEntities extends SimpleExpression {
     public ExprEntities(Library library) {
@@ -25,7 +26,7 @@ public class ExprEntities extends SimpleExpression {
     }
 
     @SuppressWarnings("unused")
-    public static Entity[] getEntities(Object type0) {
+    public static Entity[] getEntities(Object type0) throws InterruptedException, ExecutionException {
         if (type0 == null) return new Entity[0];
         final EntityType type = Skript.convert(type0, org.bukkit.entity.EntityType.class);
         Class<? extends Entity> typeClass = type.getEntityClass();
@@ -37,11 +38,9 @@ public class ExprEntities extends SimpleExpression {
         if (typeClass == null) return new Entity[0];
 
         // ByteSkript makes no guarantees regarding what thread handlers are called from. We must block here.
-        if (!MCUtil.isMainThread())
-            return Threading.forceMain(() -> getEntities(type));
-
-        return Bukkit.getWorlds().stream()
-                .flatMap(w -> w.getEntitiesByClass(typeClass).stream())
-                .toArray(Entity[]::new);
+        return Bukkit.getScheduler().callSyncMethod(JavaPlugin.getProvidingPlugin(ExprEntities.class),
+                () -> Bukkit.getWorlds().stream()
+                        .flatMap(w -> w.getEntitiesByClass(typeClass).stream())
+                        .toArray(Entity[]::new)).get();
     }
 }
